47dd63372db331d122b58b64fc71e13719124bf9
sammyizimmy <sammyizimmy@gmail.com>
2014-02-17 09:56:36 -0600

    Inital commit

---
+++ SCHtoCMD/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.xt = New System.Windows.Forms.TextBox()
+        Me.zt = New System.Windows.Forms.TextBox()
+        Me.yt = New System.Windows.Forms.TextBox()
+        Me.tt = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(154, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(126, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Convert"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'xt
+        '
+        Me.xt.Location = New System.Drawing.Point(12, 83)
+        Me.xt.Name = "xt"
+        Me.xt.Size = New System.Drawing.Size(100, 20)
+        Me.xt.TabIndex = 1
+        Me.xt.Text = "0"
+        '
+        'zt
+        '
+        Me.zt.Location = New System.Drawing.Point(224, 83)
+        Me.zt.Name = "zt"
+        Me.zt.Size = New System.Drawing.Size(100, 20)
+        Me.zt.TabIndex = 2
+        Me.zt.Text = "0"
+        '
+        'yt
+        '
+        Me.yt.Location = New System.Drawing.Point(118, 83)
+        Me.yt.Name = "yt"
+        Me.yt.Size = New System.Drawing.Size(100, 20)
+        Me.yt.TabIndex = 3
+        Me.yt.Text = "0"
+        '
+        'tt
+        '
+        Me.tt.Location = New System.Drawing.Point(330, 83)
+        Me.tt.Name = "tt"
+        Me.tt.Size = New System.Drawing.Size(100, 20)
+        Me.tt.TabIndex = 4
+        Me.tt.Text = "20"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(14, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "X"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(115, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(14, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Y"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(221, 67)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(14, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Z"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(327, 67)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Threads"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(13, 110)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(28, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "size:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(439, 131)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tt)
+        Me.Controls.Add(Me.yt)
+        Me.Controls.Add(Me.zt)
+        Me.Controls.Add(Me.xt)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents xt As System.Windows.Forms.TextBox
+    Friend WithEvents zt As System.Windows.Forms.TextBox
+    Friend WithEvents yt As System.Windows.Forms.TextBox
+    Friend WithEvents tt As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ SCHtoCMD/Form1.vb
@@ -0,0 +1,198 @@
+﻿Imports Substrate
+Imports Substrate.TileEntities
+Public Class Form1
+
+    Dim inlist As New List(Of block)
+    Dim inlistrev As New List(Of block)
+
+    Dim threads As Integer = 20
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        OpenFileDialog1.Title = "Please Select a File"
+        'OpenFileDialog1.InitialDirectory = 
+        writefile("")
+        'OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        Dim x As String = OpenFileDialog1.FileName
+        'OpenFileDialog1.Dispose()
+        readfile(x)
+    End Sub
+
+    Private Sub readfile(ByVal datfile As String)
+        ' Open our world
+        'Dim world As ImportExport.Schematic = ImportExport.Schematic.Import("C:\Users\szimmerman\Desktop\out\shack.schematic")
+        Dim world As ImportExport.Schematic = ImportExport.Schematic.Import(datfile)
+        Dim lastlist As New List(Of block)
+        Dim xdim As Integer = world.Blocks.XDim
+        Dim ydim As Integer = world.Blocks.YDim
+        Dim zdim As Integer = world.Blocks.ZDim
+        ' x, z, y is the most efficient order to scan blocks (not that
+        ' you should care about internal detail)
+        For x As Integer = 0 To xdim - 1
+            For z As Integer = 0 To zdim - 1
+                For y As Integer = 0 To ydim - 1
+                    ' Replace the block with after if it matches before
+                    If Not world.Blocks.GetID(x, y, z) = 0 Then
+                        'add to list
+                        Dim b As New block
+                        b.block = world.Blocks.GetBlock(x, y, z)
+                        b.x = x
+                        b.y = y
+                        b.z = z
+                        If issolid(b.block) Then
+                            inlist.Add(b)
+                        Else
+                            lastlist.Add(b)
+                        End If
+                    End If
+                Next
+            Next
+        Next
+
+        For Each x As block In lastlist
+            inlist.Add(x)
+        Next
+
+        For Each x As block In inlist
+            inlistrev.Add(x)
+        Next
+        inlistrev.Reverse()
+
+        Label5.Text = "size: " + Convert.ToString(threads * 2) + " wide " + Convert.ToString((inlist.Count / threads) * 2 + 10) + " long"
+
+    End Sub
+
+    Function issolid(ByVal b As AlphaBlock) As Boolean
+        Dim solids = New Integer() {1, 2, 3, 4, 5, 7, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 24, 24, 26, 30, 33, 34, 35, 36, 41, 42, 43, 44, 45, 46, 47, 48, 49, 52, 53, 54, 56, 57, 58, 60, 61, 62, 67, 73, 74, 79, 82, 84, 87, 88, 89, 95, 97, 98, 101, 102, 107, 108, 109, 110, 112, 113, 114, 116, 118, 120, 121, 123, 124, 125, 126, 128, 129, 130, 133, 134, 135, 136, 137, 138, 139, 146, 152, 153, 155, 156, 158, 159, 160, 161, 162, 163, 164, 172, 173, 174}
+        For Each x As Integer In solids
+            If b.ID = x Then Return True
+        Next
+        Return False
+    End Function
+
+
+    Private Sub writefile(ByVal datfile As String)
+        ' Open our world
+        Dim world As New ImportExport.Schematic((inlist.Count / threads) * 2 + 10, 4, threads * 2)
+
+        Dim xoffset As Integer = xt.Text
+        Dim yoffset As Integer = yt.Text
+        Dim zoffset As Integer = zt.Text
+
+        'Dim world As New ImportExport.Schematic(21, 4, 21)
+        'generate floor
+        For x As Integer = 0 To world.Blocks.XDim - 1
+            For z As Integer = 0 To world.Blocks.ZDim - 1
+                world.Blocks.SetBlock(x, 0, z, New AlphaBlock(Substrate.BlockType.SANDSTONE))
+            Next
+        Next
+
+        'generate start point
+        For x As Integer = 0 To threads - 1
+            'on cross section
+            'ss
+            'crr d
+            'ssssssss
+            'off cross section
+            '  r 
+            'crsr
+            'ssdsr
+            'ssssssss
+
+            'on stripe
+            'sandstone block
+            world.Blocks.SetBlock(6, 2, x * 2, New AlphaBlock(Substrate.BlockType.SANDSTONE))
+            'Redstone stone trail
+            world.Blocks.SetBlock(5, 1, x * 2, New AlphaBlock(Substrate.BlockType.REDSTONE_WIRE))
+            world.Blocks.SetBlock(6, 1, x * 2, New AlphaBlock(Substrate.BlockType.REDSTONE_WIRE))
+            'Repeater
+            world.Blocks.SetBlock(3, 1, x * 2, New AlphaBlock(Substrate.BlockType.REDSTONE_REPEATER_ON, (RepeaterDelay.DELAY_1 Or RepeaterOrientation.WEST)))
+
+            'off stripe
+            world.Blocks.SetBlock(6, 1, x * 2 + 1, New AlphaBlock(Substrate.BlockType.SANDSTONE))
+            world.Blocks.SetBlock(5, 2, x * 2 + 1, New AlphaBlock(Substrate.BlockType.SANDSTONE))
+            world.Blocks.SetBlock(4, 1, x * 2 + 1, New AlphaBlock(Substrate.BlockType.SANDSTONE))
+            'Redstone stone trail
+            world.Blocks.SetBlock(6, 2, x * 2 + 1, New AlphaBlock(Substrate.BlockType.REDSTONE_WIRE))
+            world.Blocks.SetBlock(5, 3, x * 2 + 1, New AlphaBlock(Substrate.BlockType.REDSTONE_WIRE))
+            world.Blocks.SetBlock(4, 2, x * 2 + 1, New AlphaBlock(Substrate.BlockType.REDSTONE_WIRE))
+            world.Blocks.SetBlock(3, 1, x * 2 + 1, New AlphaBlock(Substrate.BlockType.REDSTONE_WIRE))
+            'Repeater
+            world.Blocks.SetBlock(5, 1, x * 2 + 1, New AlphaBlock(Substrate.BlockType.REDSTONE_REPEATER_ON, (RepeaterDelay.DELAY_1 Or RepeaterOrientation.WEST)))
+        Next
+
+        Dim counter As Integer = 1
+        For Each blk As block In inlist
+
+            Dim row As Integer = Math.Ceiling(counter / (threads))
+
+            'on stripe
+            'sandstone block
+            Dim x = 7 + ((row - 1) * 2)
+            Dim z = ((counter - 1) - ((row - 1) * threads)) * 2
+
+            world.Blocks.SetBlock(x, 2, z, New AlphaBlock(Substrate.BlockType.SANDSTONE))
+            world.Blocks.SetBlock(x + 1, 2, z, New AlphaBlock(Substrate.BlockType.SANDSTONE))
+            'Repeater
+            world.Blocks.SetBlock(x + 1, 1, z, New AlphaBlock(Substrate.BlockType.REDSTONE_REPEATER_ON, (RepeaterDelay.DELAY_1 Or RepeaterOrientation.SOUTH)))
+
+            'off stripe
+            world.Blocks.SetBlock(x, 1, z + 1, New AlphaBlock(Substrate.BlockType.SANDSTONE))
+            world.Blocks.SetBlock(x + 1, 1, z + 1, New AlphaBlock(Substrate.BlockType.SANDSTONE))
+            'Repeater
+            world.Blocks.SetBlock(x + 1, 2, z + 1, New AlphaBlock(Substrate.BlockType.REDSTONE_REPEATER_ON, (RepeaterDelay.DELAY_1 Or RepeaterOrientation.SOUTH)))
+
+            'on
+            Dim ab1 As New Substrate.AlphaBlock(BlockType.COMMAND_BLOCK)
+            Dim xx1 As TileEntityControl = ab1.GetTileEntity
+            xx1.Command = "setblock " + Convert.ToString(blk.x + xoffset) + " " + Convert.ToString(blk.y + yoffset) + " " + Convert.ToString(blk.z + zoffset) + " " + Convert.ToString(blk.block.ID) + " " + Convert.ToString(blk.block.Data)
+            ab1.SetTileEntity(xx1)
+            world.Blocks.SetBlock(x, 1, z, ab1)
+            'off
+            Dim ab2 As New Substrate.AlphaBlock(BlockType.COMMAND_BLOCK)
+            Dim xx2 As TileEntityControl = ab2.GetTileEntity
+            xx2.Command = "setblock " + Convert.ToString(inlistrev(counter - 1).x + xoffset) + " " + Convert.ToString(inlistrev(counter - 1).y + yoffset) + " " + Convert.ToString(inlistrev(counter - 1).z + zoffset) + " 00"
+            ab2.SetTileEntity(xx2)
+            world.Blocks.SetBlock(x, 2, z + 1, ab2)
+
+            counter += 1
+        Next
+
+        Dim saveFileDialog1 As New SaveFileDialog()
+
+        saveFileDialog1.Filter = "schematic files (*.schematic)|*.schematic|All files (*.*)|*.*"
+        saveFileDialog1.FilterIndex = 2
+        saveFileDialog1.RestoreDirectory = True
+
+        If saveFileDialog1.ShowDialog() = DialogResult.OK Then
+            If Not saveFileDialog1.FileName.Contains(".schematic") Then
+                world.Export(saveFileDialog1.FileName + ".schematic")
+            Else
+                world.Export(saveFileDialog1.FileName)
+            End If
+        End If
+
+        Me.Close()
+    End Sub
+
+    Class block
+        Public block As AlphaBlock
+        Public x As Integer
+        Public y As Integer
+        Public z As Integer
+    End Class
+
+
+    Private Sub TextBox4_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tt.TextChanged
+        threads = tt.Text
+        Label5.Text = "size: " + Convert.ToString(threads * 2) + " wide " + Convert.ToString((inlist.Count / threads) * 2 + 10) + " long"
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        OpenFileDialog1.Title = "Please Select a File"
+        OpenFileDialog1.ShowDialog()
+    End Sub
+End Class
